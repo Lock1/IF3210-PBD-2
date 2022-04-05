ab0e5205b70d7599ffc55ffdd41ce049314b0a93
fadelananda <fadelananda123@gmail.com>
2022-04-06 00:32:51 +0700

    add play zen and play wave mode

--- Survival Shooter Extended++/Assets/MainMenu.cs
+++ Survival Shooter Extended++/Assets/MainMenu.cs
@@ -5,9 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame ()
+    public void PlayZen ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayWave ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void QuitGame ()
